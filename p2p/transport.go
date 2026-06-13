0f168be6dce5eb22742989cbf9e592c5fe867eda
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-06-13 11:54:28 +0200

    feat: add peer and transport interfaces

---
+++ p2p/transport.go
@@ -0,0 +1,24 @@
+package p2p
+
+import (
+	"io"
+	"net"
+)
+
+// Peer is an interface that represents the remote node
+type Peer interface {
+    net.Conn
+    Send(t byte, r io.Reader, size int64) error
+    CloseStream()
+    ReadStream(size int64) io.Reader
+}
+
+// Transport is anything that handles the communication
+// between nodes in the network
+// This can be of form TCP, UDP, or websockets
+type Transport interface {
+    ListenAndAccept() error
+    Dial(string) error
+    Consume() <-chan RPC
+    Close() error
+}
